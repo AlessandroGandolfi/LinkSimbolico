62541e2726475ca3255df0c8d079ff55b81a1d89
Alessandro Gandolfi <alessandro.gandolfi@vallauri.edu>
2016-07-07 15:23:34 +0200

    Versione 1.1: risolto il problema degli spazi nei nomi delle cartelle

--- CL.bat
+++ CL.bat
@@ -1,10 +1,11 @@
 @echo off
-set sorgente=%1
-set destinazione=%2
-set file=%3
+rem genero dei "short path" in modo che eventuali spazi nei path delle cartelle non influiscano nell'esecuzione dei comandi
+set sorgente=%~s1
+set destinazione=%~s2
+set estensione=%3
 rem /P è un parametro che specifica il percorso in cui il comando forfiles viene eseguito
 rem /M è il filtro usato per prendere in considerazione file con specifiche estensioni o nomi
 rem /C esegue il comando presente nella stringa che lo segue
-forfiles /P "%sorgente%" /M %file% /C "cmd /c mklink \"%destinazione%\@file\" @path"
+forfiles /P %sorgente% /M %estensione% /C "cmd /c mklink \"%destinazione%\@file\" @path"
 start %destinazione%
 pause
